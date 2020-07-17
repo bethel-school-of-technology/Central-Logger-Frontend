2061bdc666a5ba42fd5289273895950cfe152e98
unknown <tatefowble@gmail.com>
2020-07-17 17:27:17 -0500

    First Stage

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+ {
+   path:'list'
+ }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
